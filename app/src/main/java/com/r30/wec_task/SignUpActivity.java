f72b8b839a3714304c8d0e86dfd8ceb14bbf1a0d
addhyanmalhotra <addhyanmalhotra@gmail.com>
2020-10-01 23:01:38 +0530

    Added Contact picker for primary emergency contact

--- app/src/main/java/com/r30/wec_task/SignUpActivity.java
+++ app/src/main/java/com/r30/wec_task/SignUpActivity.java
@@ -1,19 +1,23 @@
 package com.r30.wec_task;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
@@ -21,10 +25,12 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUpActivity extends AppCompatActivity {
+    private static final int RESULT_PICK_CONTACT = 101;
     public TextInputEditText emailId, passwd,uName,uPhone,ePhone;
     Button btnSignUp;
     TextView signIn;
     FirebaseAuth firebaseAuth;
+    TextInputLayout eConTF;
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference users = database.getReference("users");
 
@@ -36,10 +42,19 @@ public class SignUpActivity extends AppCompatActivity {
         emailId = findViewById(R.id.emailId);
         passwd = findViewById(R.id.password);
         uName = findViewById(R.id.fullname);
+        eConTF = findViewById(R.id.ePhoneTF);
         uPhone=findViewById(R.id.phoneNo);
         ePhone=findViewById(R.id.EmergencyphoneNo);
         btnSignUp = findViewById(R.id.signUpBtn2);
         signIn = findViewById(R.id.TVSignIn);
+        eConTF.setEndIconOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent contactPickerIntent = new Intent(Intent.ACTION_PICK,
+                        ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
+                startActivityForResult(contactPickerIntent, RESULT_PICK_CONTACT);
+            }
+        });
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -101,5 +116,38 @@ public class SignUpActivity extends AppCompatActivity {
                 startActivity(I);
             }
         });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case RESULT_PICK_CONTACT:
+                    Cursor cursor = null;
+                    try {
+                        String contactNumber = null;
+                        String contactName = null;
+                        // getData() method will have the
+                        // Content Uri of the selected contact
+                        Uri uri = data.getData();
+                        //Query the content uri
+                        cursor = getContentResolver().query(uri, null, null, null, null);
+                        cursor.moveToFirst();
+                        // column index of the phone number
+                        int phoneIndex = cursor.getColumnIndex(
+                                ContactsContract.CommonDataKinds.Phone.NUMBER);
+                        // column index of the contact name
+                        int nameIndex = cursor.getColumnIndex(
+                                ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+                        contactNumber = cursor.getString(phoneIndex);
+                        contactName = cursor.getString(nameIndex);
+                        // Set the value to the textviews
+                        ePhone.setText("Contact Number : ".concat(contactNumber));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
+            }
+        }
     }
 }
